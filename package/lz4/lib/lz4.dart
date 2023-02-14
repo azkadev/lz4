798700e6470c1c27188ecf3181cc91d621fa0585
￴ <>
2023-02-15 00:25:37 +0700

    udpate

--- package/lz4/lib/lz4.dart
+++ package/lz4/lib/lz4.dart
@@ -3,6 +3,9 @@
 /// More dartdocs go here.
 library lz4;
 
+import "package:universal_io/io.dart";
+
 export 'src/lz4_base.dart';
+
 
 // TODO: Export any libraries intended for clients of this package.
